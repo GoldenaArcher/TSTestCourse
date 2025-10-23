169074fdb876afecf3868fcd4769a32141caed15
GoldenaArcher <25376645+GoldenaArcher@users.noreply.github.com>
2025-10-23 04:42:21 -0400

    Refactor utils tests to improve structure and clarity

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,21 @@
 export const toUpperCase = (str: string): string => {
   return str.toUpperCase();
 };
+
+export type StringInfo = {
+  length: number;
+  lowercase: string;
+  uppercase: string;
+  characters: string[];
+  extraInfo: Record<string, any>;
+};
+
+export const getStringInfo = (arg: string): StringInfo => {
+  return {
+    length: arg.length,
+    lowercase: arg.toLowerCase(),
+    uppercase: arg.toUpperCase(),
+    characters: arg.split(""),
+    extraInfo: {},
+  };
+};
--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,7 +1,35 @@
-import { toUpperCase } from "../app/utils";
+import { getStringInfo, toUpperCase } from "../app/utils";
 
 describe("Utils", () => {
   it("should convert string to uppercase", () => {
-    expect(toUpperCase("hello")).toBe("HELLO");
+    // arrange
+    const sut = toUpperCase; // system under test
+    const expected = "HELLO";
+
+    // act
+    const actual = sut("hello");
+
+    // assert
+    expect(actual).toBe(expected);
+  });
+
+  it("should return info for valid string", () => {
+    const sut = getStringInfo;
+    const actual = sut("My-String");
+
+    expect(actual.lowercase).toBe("my-string");
+    expect(actual.uppercase).toBe("MY-STRING");
+    expect(actual).toHaveLength(9);
+
+    expect(actual.characters).toEqual(["M", "y", "-", "S", "t", "r", "i", "n", "g"]);
+    expect(actual.characters).toContain<string>("M");
+    expect(actual.characters).toEqual(
+        expect.arrayContaining(["M", "y", "S", "t", "r", "i", "n", "g", "-"])
+    )
+
+    expect(actual.extraInfo).toBeTruthy();
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toEqual({});
   });
 });
